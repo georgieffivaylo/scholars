2bac80dc451d882a234f7d160bdcd9d195e7984d
DESKTOP-34MI82R\HP <ivaylo.georgieff@yahoo.com>
2026-06-13 10:57:04 +0300

    Add fallback controller endpoint

--- src/main/java/eu/leadconsult/scholars/module/scholar/controller/ScholarMvcController.java
+++ src/main/java/eu/leadconsult/scholars/module/scholar/controller/ScholarMvcController.java
@@ -1,8 +1,12 @@
 package eu.leadconsult.scholars.module.scholar.controller;
 
+import eu.leadconsult.scholars.exception.ScholarException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Controller
 @AllArgsConstructor
@@ -11,5 +15,14 @@ public class ScholarMvcController {
     @GetMapping("/scholar")
     public String getScholarLandingPage() {
         return "scholar-landing-page";
+    }
+
+    @GetMapping("/")
+    public void getScholarIndex(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/scholar");
+        } catch (IOException e) {
+            throw new ScholarException("Error redirecting to scholar landing page", null);
+        }
     }
 }
